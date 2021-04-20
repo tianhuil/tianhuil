e0a7d30b4f859d9b33d8b9b1107e9694d1630a14
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2021-04-20 10:30:11 -0700

    Fix build

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,9 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+declare module 'react-social-icons' {
+  export class SocialIcon extends React.Component<
+    { className: string; url: string },
+    any
+  > {}
+}
