2a18ebcbef37a5f13e3b23415d054925b36d7407
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2023-06-05 11:14:31 -0400

    Update config

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,9 +1,5 @@
 /// <reference types="next" />
-/// <reference types="next/types/global" />
+/// <reference types="next/image-types/global" />
 
-declare module 'react-social-icons' {
-  export class SocialIcon extends React.Component<
-    { className: string; url: string },
-    any
-  > {}
-}
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/basic-features/typescript for more information.
